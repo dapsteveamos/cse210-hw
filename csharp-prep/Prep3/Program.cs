13482e39c489383697b8798fc491d9ced4b638a6
Steve Amos Daprim <dapsteveamos@gmail.com>
2024-09-22 21:00:48 +0100

    First prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes"; // Initialize the playAgain variable
+        
+        while (playAgain.ToLower() == "yes") // Loop for replaying the game
+        {
+            // For Part 3, where we use a random number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0; // Initialize the guess counter
+
+            // We could also use a do-while loop here...
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++; // Increment the guess counter
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses."); // Display guess count
+                }
+            }
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
