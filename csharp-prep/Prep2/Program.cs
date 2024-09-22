a757aecaa2e911843e788b80ee228bd5fab4294d
Steve Amos Daprim <dapsteveamos@gmail.com>
2024-09-22 20:42:47 +0100

    Finish prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the letter grade
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign (+ or -)
+        int lastDigit = percent % 10; // Get the last digit
+
+        if (letter != "A" && letter != "F") // Only apply signs for B, C, D grades
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && percent < 94) // A can only have a '-'
+        {
+            if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Display the grade with sign
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Pass/fail message
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
